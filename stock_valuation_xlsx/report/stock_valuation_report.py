fca83d378daf1e61c3c26b058bbb77c000af8eab
Kinner Vachhani <kenvac@users.noreply.github.com>
2018-12-04 00:13:35 +0000

    [IMP][8.0] Add seller column to xlsx report

--- stock_valuation_xlsx/report/stock_valuation_report.py
+++ stock_valuation_xlsx/report/stock_valuation_report.py
@@ -16,17 +16,20 @@ class StockValuationXlsx(ReportXlsx):
         bold = workbook.add_format({'bold': True})
         sheet.write(0, 0, "Product Name", bold)
         sheet.write(0, 1, "Product Code", bold)
-        sheet.write(0, 2, "Unit Cost", bold)
-        sheet.write(0, 3, "Qty on hand", bold)
-        sheet.write(0, 4, "Valuation", bold)
+        sheet.write(0, 2, "Seller", bold)
+        sheet.write(0, 3, "Unit Cost", bold)
+        sheet.write(0, 4, "Qty on hand", bold)
+        sheet.write(0, 5, "Valuation", bold)
+
         row = 1
         for obj in data['form']:
             # One sheet by partner
             sheet.write(row, 0, obj['name'])
             sheet.write(row, 1, obj['code'])
-            sheet.write(row, 2, obj['cost'])
-            sheet.write(row, 3, obj['qty'])
-            sheet.write(row, 4, obj['valuation'])
+            sheet.write(row, 2, obj['seller'])
+            sheet.write(row, 3, obj['cost'])
+            sheet.write(row, 4, obj['qty'])
+            sheet.write(row, 5, obj['valuation'])
             row += 1
 
 
