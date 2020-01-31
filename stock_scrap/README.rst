7dcb8dbca6b3774ede7f5a832a0ebd04c3fb31a0
Kinner Vachhani <kenvac@users.noreply.github.com>
2020-01-31 13:57:19 +0000

    [MIG] : Migration to 12.0 (#18)

---
+++ stock_scrap/README.rst
@@ -0,0 +1,27 @@
+.. image:: https://img.shields.io/badge/licence-LGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/lgpl
+   :alt: License: LGPL-3
+
+================
+Stock Scrap Cost
+================
+
+This module extends scrap functionality
+of stock module and adds cost and total
+total of scrap on stock.scrap.
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/kenvac/odoo-extras/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smash it by providing detailed and welcomed feedback.
+
+
+Contributors
+============
+
+* Kinner Vachhani
+
+Do not contact contributors directly about support or help with technical issues.
---
+++ stock_scrap/models/stock_scrap.py
@@ -0,0 +1,35 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo import models, api, fields
+
+
+class StockScrap(models.Model):
+    _inherit = 'stock.scrap'
+
+    @api.depends('scrap_qty', 'product_id', 'cost')
+    def _compute_total_cost(self):
+        for record in self:
+            if record.product_id:
+                record.total_cost = (record.scrap_qty or 0.0) * record.cost
+            else:
+                record.total_cost = record.cost = 0.0
+
+    cost = fields.Float('Unit Cost Price', default=0.0,
+                        states={'done': [('readonly', True)]},
+                        help="Cost Price per unit")
+    total_cost = fields.Float(help="""Total Scap Total. Total """
+                                   """Scrap quantity * Unit Cost price""",
+                              compute='_compute_total_cost',
+                              store=True)
+
+    @api.onchange('cost', 'scrap_qty')
+    def _onchange_cost(self):
+        if self.product_id:
+            self.total_cost = self.scrap_qty * self.cost
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        self.total_cost = self.cost = 0.0
+        if self.product_id:
+            self.cost = self.product_id.standard_price
+            self.total_cost = self.scrap_qty * self.cost
